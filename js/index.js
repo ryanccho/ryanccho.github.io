c7d34fac831ec173f169dc9a7baee63eb89f84d7
Ryan Cho <cho.ryan.96@gmail.com>
2025-02-14 16:50:38 -0800

    dynamic viewport height test

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("top_button").addEventListener("click", () => {
+    console.log("foo bar");
+});
+
+document.getElementById("bottom_button").addEventListener("click", () => {
+    console.log("foo bar");
+});
